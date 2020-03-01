217f0332a759ac3f727d99dc8706d6f0dfd385c6
David <dhalsey212@gmail.com>
2020-03-01 17:18:14 -0500

    Move react & redux over to create react app

---
+++ src/components/home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import bowies from "../assets/changes.gif";
+import {AllBowies} from './AllBowies'
+
+export const Home = () => {
+  return (
+    <div>
+      <div id="header">
+        <div className="webHead">
+          so I turn myself to<div className="headAccent">FACE ME</div>
+        </div>
+        <img
+          className="bowiegif" 
+          src={bowies}
+          alt="changes"
+        />
+      </div>
+      <div>
+        <AllBowies/>
+      </div>
+    </div>
+  );
+};
