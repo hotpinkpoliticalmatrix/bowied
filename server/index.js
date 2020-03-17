b1a1b7ee1ad66a1f145f58812652f4ed52afc61e
Stacey Eliuk <stacey.eliuk@gmail.com>
2020-03-16 23:19:14 -0400

    updated server file

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const { db } = require('./db')
+const app = require('..')
+const PORT = 8080
+
+db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
+  .then(() => {
+    console.log('db synced')
+    app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
+  })
